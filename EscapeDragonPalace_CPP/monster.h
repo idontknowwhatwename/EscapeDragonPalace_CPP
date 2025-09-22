2eb72347ae50cc97d2a50bbf6261f8db2191188a
wlgus123 <mail30019794@gmail.com>
2025-09-22 17:33:12 +0900

    몬스터, 꽃게 클래스 구현

--- EscapeDragonPalace_CPP/monster.h
+++ EscapeDragonPalace_CPP/monster.h
@@ -3,6 +3,10 @@
 // 몬스터 클래스
 class Monster
 {
+public: 
+	// 생성자
+	Monster();
+	virtual ~Monster();
 protected:
 	int HP;						// 체력
 	int attack;					// 공격력
@@ -10,6 +14,12 @@ protected:
 	bool isAlive;				// 생존 여부
 	bool isDamaged;				// 피격 당한 상태
 	unsigned long lastHitTime;	// 마지막 피격 시간
+public:
+	// 함수
+	virtual void UpdateMonster();	// 업데이트
+	void DrawMonster();		// 그리기
+	virtual bool MonsterHitPlayer();	// 몬스터 -> 플레이어 피격처리
+	virtual bool PlayerHitMonster();	// 플레이어 -> 몬스터 공격처리
 };
 
 // 몬스터 타입 열거형
